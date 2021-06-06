26db49b668df6fc18c619d993f6905a4c7215831
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-05 21:27:17 -0400

    Show con TDD

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -43,6 +44,7 @@ class PostControllerTest extends TestCase
 
         $this->assertDatabaseHas('posts', ['title' => 'Post de Prueba']);
     }
+
     public function test_validate_title()
     {
         $response = $this->json('POST', '/api/posts', [
@@ -51,5 +53,24 @@ class PostControllerTest extends TestCase
         // Estatus HTTP 422 no se pudo completar la solicitud
         $response->assertStatus(422)
             ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200);
+    }
+    public function test_404_show()
+    {
+        $rand = rand();
+
+        $response = $this->json('GET', "/api/posts/$rand");
+
+        $response->assertStatus(404);
     }
 }
