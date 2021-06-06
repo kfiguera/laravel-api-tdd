99fff540e1db831e86eef9ec8646af868a7187a8
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-05 20:20:36 -0400

    Configurando el método Store con TDD

--- database/migrations/2021_06_05_211301_create_posts_table.php
+++ database/migrations/2021_06_05_211301_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->comment('Título del Post');
             $table->timestamps();
         });
     }
--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,39 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * Prueba del Metodo Store de POST
+     */
+    public function test_store()
+    {
+        /*
+         * Verificar los Errores
+         */
+        //$this->withoutExceptionHandling();
+
+        /*
+         * Prueba de Envio mediante POST el Titulo en formato JSON
+         */
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'Post de Prueba',
+
+        ]);
+        /*
+         * Validaciones
+         * Estructura del Json de Respuesta que posea: id, title, create_at, updated_at
+         * Verificar que el Json de respuesta contenga el título enviado
+         * Verificar Estatus de respuesta sea igual a 201
+         */
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'Post de Prueba'])
+            ->assertStatus(201); // Ok creado un recurso
+        /*
+         * Validaciones
+         * Verificar si en Base de datos existe un registro con la información enviada
+         */
+
+        $this->assertDatabaseHas('posts', ['title' => 'Post de Prueba']);
     }
 }
